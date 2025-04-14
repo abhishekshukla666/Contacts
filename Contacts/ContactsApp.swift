d90e504e0524b40fad1ec766e71afe83e7f5eddb
Abhishek Shukla <abhishekshuklaa666@gmail.com>
2025-04-14 20:05:39 +0530

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Abhishek Shukla on 14/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
